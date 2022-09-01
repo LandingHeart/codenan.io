6f06c9b31c1d3d896e0b6a808b2b2456bf6e77b4
LandingHeart <shinan.liu@yahoo.com>
2022-09-01 10:42:10 -0400

    save, navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from "./logo.svg";
 import "./App.css";
-
+import NavBar from "./components/NavBar";
 function App() {
   return (
-    <div className="App">
-      <h1> To Be Continued</h1>
+    <div>
+      <NavBar />
+      <center>In progress...</center>
     </div>
   );
 }
